8a403d171c08f1fe1d88a0b8a1020170a3a3b294
HumbalAli <160706785+HumbalAli@users.noreply.github.com>
2024-02-29 02:17:25 +0500

    Add files via upload

---
+++ 05.js
@@ -0,0 +1,13 @@
+"use strict";
+let num1 = 10;
+let num2 = 2;
+console.log(num1 - num2);
+let num3 = 6;
+let num4 = 2;
+console.log(num3 + num4);
+let num5 = 4;
+let num6 = 2;
+console.log(num5 * num6);
+let num7 = 16;
+let num8 = 2;
+console.log(num7 / num8);
