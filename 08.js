6cad85d0e7e9baa5872a6611ee9529aa6db5dd18
HumbalAli <160706785+HumbalAli@users.noreply.github.com>
2024-03-01 15:36:35 +0500

    Add files via upload

---
+++ 08.js
@@ -0,0 +1,6 @@
+"use strict";
+//hello my name is Humbal Ali 
+//this program can show your favourite number in terminal like it shows mine 
+let favouriteNumber = "7,";
+let message = (favouriteNumber) + "is my favourite number";
+console.log(message);
---
+++ 09.js
@@ -0,0 +1,7 @@
+"use strict";
+//Hello my name is Humbal Ali
+//this program shows the awnser of these equations 
+console.log(5 + 3);
+console.log(2 * 4);
+console.log(10 - 2);
+console.log(16 / 2);
