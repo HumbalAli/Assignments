7a80909ec8e528e295a0c71808920a2f7aa93963
Humbal Ali <160706785+HumbalAli@users.noreply.github.com>
2024-05-03 01:47:51 +0500

    Add files via upload

---
+++ 10.js
@@ -0,0 +1,5 @@
+"use strict";
+let names = ["abdullah", "babar", "hassan", "sarim"];
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
---
+++ 10.ts
@@ -0,0 +1,5 @@
+let names: string[] = ["abdullah", "babar", "hassan", "sarim"];
+
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
