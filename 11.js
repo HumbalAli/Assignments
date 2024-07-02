df5ea76de25d6421d12b5a31ddef7aed6ee2a5a1
Humbal Ali <160706785+HumbalAli@users.noreply.github.com>
2024-07-02 20:27:15 +0500

    Add files via upload

---
+++ 11.js
@@ -0,0 +1,6 @@
+"use strict";
+// this is program that greets indivisually
+let names = ["abdullah", "babar", "hassan", "sarim"];
+for (let name of names) {
+    console.log(`hello, ${name}! I hope you are good.`);
+}
---
+++ 11.ts
@@ -0,0 +1,25 @@
+// this is program that greets indivisually
+
+
+let names: string[] = ["abdullah", "babar", "hassan", "sarim"];
+
+for (let name of names) {
+    console.log(`hello, ${name}! I hope you are good.`)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
